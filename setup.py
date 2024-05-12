6885ce1b307f182cae50e303e723b6f7e093b7c0
MZhafran <37040500+iamzhaf@users.noreply.github.com>
2024-05-12 16:36:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='utils',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        numpy,
+        pandas,
+        scipy,
+        tabulate
+        
+    ],
+)
